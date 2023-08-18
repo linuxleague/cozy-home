2be64c5e8b9f00bdd8fecf13bbf94a04bd4f2b25
Crash-- <quentin.valmori@gmail.com>
2023-08-18 10:11:04 +0200

    fix: Tmp commit since there is a bug in the stack
    Stack returns twice the app when served by --serve option in dev mode
    resulting in a lot of warning in the console. Made a quick dedup

--- src/components/Applications.jsx
+++ src/components/Applications.jsx
@@ -1,6 +1,6 @@
 import React, { memo, useEffect, useRef } from 'react'
 import memoize from 'lodash/memoize'
-
+import uniqBy from 'lodash/uniqBy'
 import { useQuery } from 'cozy-client'
 import flag from 'cozy-flags'
 import Divider from 'cozy-ui/transpiled/react/Divider'
@@ -47,10 +47,10 @@ const getApplicationsList = memoize(data => {
         !homeConfig.filteredApps.includes(app.slug) &&
         !flag(`home_hidden_apps.${app.slug.toLowerCase()}`) // can be set in the context with `home_hidden_apps: - drive - banks`for example
     )
+    const dedupapps = uniqBy(apps, 'slug')
+    const array = dedupapps.map(app => <AppTile key={app.id} app={app} />)
 
-    const array = apps.map(app => <AppTile key={app.id} app={app} />)
-
-    array.push(<BackupAppHighlightAlert apps={apps} />)
+    array.push(<BackupAppHighlightAlert apps={dedupapps} />)
 
     return array
   } else {
